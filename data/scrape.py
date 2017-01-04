f16f273487a9557bad8fae43bb55217fad4ffa1c
Luke O'Sullivan <osulluke@gmail.com>
2017-01-03 22:36:35 -0700

    Add govtracker webscraper + hyperlink data.
    Simple tool scrapes the hyperlink data from our own webpage, and gets the politician's name associated with it.

---
+++ data/scrape.py
@@ -0,0 +1,10 @@
+import bs4, requests #libraries for web-scraping
+
+#variable set up
+res = requests.get('https://codefortucson.github.io/govtracker-az/')
+soup = bs4.BeautifulSoup(res.text,'html.parser')
+links = soup.find_all('a')
+
+#loop through data to pick out hypertext refs and name fields
+for link in links:
+    print(link.get('href'), link.text, sep=' "', end='"\n')
